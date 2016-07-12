5e46c33a5870c9241f7d6ccc0cfcbb5877806bfb
Rick Liu <liuzhipassman@126.com>
2016-07-12 17:20:09 +0800

    Refactor DaoRowMapper

--- LotteryService/src/main/java/cn/wisdom/lottery/payment/dao/mapper/DaoRowMapper.java
+++ LotteryService/src/main/java/cn/wisdom/lottery/payment/dao/mapper/DaoRowMapper.java
@@ -3,6 +3,8 @@ package cn.wisdom.lottery.payment.dao.mapper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.sql.Clob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -35,6 +37,14 @@ public class DaoRowMapper<T> implements RowMapper<T>
 {
     private Logger logger = LoggerFactory.getLogger(DaoRowMapper.class
             .getName());
+    
+    public DaoRowMapper()
+    {
+    	rowObjClass = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+
+        this.direct = this.isDirectClass();
+    }
 
     public DaoRowMapper(Class<? extends T> rowObjClass)
     {
