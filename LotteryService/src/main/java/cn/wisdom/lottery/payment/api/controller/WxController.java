df3c8519065b341a3307592e230131cdbdc72cd7
Rick Liu <liuzhipassman@126.com>
2016-07-13 17:39:06 +0800

    oauth2

--- LotteryService/src/main/java/cn/wisdom/lottery/payment/api/controller/WxController.java
+++ LotteryService/src/main/java/cn/wisdom/lottery/payment/api/controller/WxController.java
@@ -3,15 +3,24 @@ package cn.wisdom.lottery.payment.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.wisdom.lottery.payment.common.utils.HttpUtils;
+import cn.wisdom.lottery.payment.service.wx.WXService;
+
 @RequestMapping("/wx")
 @Controller
-public class WeiXinController {
+public class WxController {
+	
+	@Autowired
+	private WXService wxService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/verify1")
 	public String wxVerify(String signature, String token, String timestamp, String nonce,
@@ -29,4 +38,14 @@ public class WeiXinController {
 	public String wxVerify(@RequestParam String echostr) {
 		return echostr;
 	}
+    
+    @RequestMapping(method = RequestMethod.GET, value = "/oauth2")
+    @ResponseBody
+    public String oauth2(HttpServletRequest request) {
+    	
+    	String code = HttpUtils.getParamValue(request, "code");
+    	String state = HttpUtils.getParamValue(request, "state");
+    	
+    	return echostr;
+    }
 }
